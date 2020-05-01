ba69c1ecf8a0eb5e7911cd9a0b24d00b559c8315
Leon <leon.kamerlin@dings.solutions>
2020-05-01 22:27:27 +0200

    update

--- projects/stripe-elements-example/src/app/app.module.ts
+++ projects/stripe-elements-example/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { StripeElementsConfig, StripeElementsModule } from 'stripe-elements';
 import { environment } from '../environments/environment';
+import ElementsOptions = stripe.elements.ElementsOptions;
 
 const style = {
     base: {
@@ -13,10 +14,15 @@ const style = {
         fontSize: '19px',
     }
 };
+
+const elementsOptions: ElementsOptions = {
+    style,
+    placeholderCountry: 'es'
+};
 const stripeConfig: StripeElementsConfig = {
     stripeKey: environment.stripeKey,
     api: environment.firebaseEndpoint,
-    elementOptions: {style}
+    elementOptions: elementsOptions
 };
 
 @NgModule({
