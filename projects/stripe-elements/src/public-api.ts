040dc07983e375690b8d5b3b646012de49ae8063
Leon <leonkamerlinn@gmail.com>
2020-05-03 18:29:54 +0200

    exporting new module

--- projects/stripe-elements/src/public-api.ts
+++ projects/stripe-elements/src/public-api.ts
@@ -5,3 +5,5 @@
 export * from './lib/stripe-elements.service';
 export * from './lib/stripe-elements.component';
 export * from './lib/stripe-elements.module';
+export * from './lib/stripe-form/stripe-form.component';
+export * from './lib/stripe-form/stripe-form.module';
